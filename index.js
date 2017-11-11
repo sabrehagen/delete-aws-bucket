4e576c0c8d5d20011542e6af31dda44cebd71910
Jackson Delahunt <jackson@stemn.com>
2017-11-11 10:22:43 +1000

    add global error handler

--- index.js
+++ index.js
@@ -2,13 +2,33 @@ const emptyBucket = require('empty-aws-bucket');
 const aws = require('aws-sdk');
 const s3 = new aws.S3();
 
-const deleteBucket = async (Bucket) => {
+const deleteBucket = async (Bucket, options) => {
+
+  if (options) {
+    aws.config.update(options);
+  }
+
+  // if AWS SDK debugging is enabled, log our actions
+  const debug = (...args) => {
+    if (process.env.AWSJS_DEBUG) {
+      console.log(`delete-s3-bucket | ${Bucket} |`, ...args);
+    }
+  }
 
   // before we can delete the bucket, we must delete all versions of all objects
-  await emptyBucket(Bucket);
+  await emptyBucket(Bucket, options);
 
   // finally, delete the empty bucket
-  await s3.deleteBucket({ Bucket }).promise();
+  debug('Deleting bucket');
+  try {
+    await s3.deleteBucket({ Bucket }).promise();
+  } catch (e) {
+    if (e.code === 'NoSuchBucket') {
+      debug('Bucket not found!');
+    } else {
+      throw e;
+    }
+  }
 };
 
 module.exports = deleteBucket;
